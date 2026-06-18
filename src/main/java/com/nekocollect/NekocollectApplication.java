934595486dfcb4c6f69f2e6ce84126513cfed991
LORD_D3V <Felipe.dev.jogos@gmail.com>
2026-06-18 19:11:21 -0300

    Signed-off-by: LORD_D3V <Felipe.dev.jogos@gmail.com>

---
+++ src/main/java/com/nekocollect/NekocollectApplication.java
@@ -0,0 +1,13 @@
+package com.nekocollect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekocollectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NekocollectApplication.class, args);
+	}
+
+}
